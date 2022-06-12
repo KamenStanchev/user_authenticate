f95f3a2299d2ce8b040dc268b1848d1fb89c4dbc
Kamen Stanchev <k.s@abv.bg>
2022-06-12 15:16:16 +0300

    log in log out

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -4,8 +4,11 @@ from django.urls import path
 from authenticate.views import home
 
 
-from authenticate.views import home
+from authenticate.views import home, signup, signin, signout
 
 urlpatterns = [
     path('', home, name='home'),
+    path('signup/', signup, name='signup_page'),
+    path('signin/', signin, name='signin_page'),
+    path('signout/', signout, name='signout_page'),
 ]
--- authenticate/views.py
+++ authenticate/views.py
@@ -1,6 +1,53 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 
 def home(request):
-
     return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        my_user = User.objects.create_user(username, email, pass1)
+        my_user.first_name = fname
+        my_user.last_name = lname
+
+        my_user.save()
+
+        messages.success(request, 'Your account has been successful created.')
+        return redirect('signin_page')
+
+    return render(request, 'signup.html')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, 'home.html', {'fname': fname})
+        else:
+            messages.error(request, 'Bad Credentials')
+            return redirect('home')
+
+    return render(request, 'signin.html')
+
+
+def signout(request):
+    logout(request)
+    messages.success(request, 'You are successful Sign-OUT')
+    return redirect('home')
